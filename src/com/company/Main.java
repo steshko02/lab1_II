d9d03c7d593ced3cdc5466bf1e7f93361b3e0b96
Alex Steshko <steshko02@mail.ru>
2022-11-11 14:10:27 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Process process = new Process();
+        process.process();
+//
+//        switch (command){
+//           case 1: {
+//                System.out.println("1) Просмотреть все характеристики.");
+//
+//
+//                break;
+//            }
+//           case  2:
+//                System.out.println("1) Просмотреть все объекты.");
+//                break;
+//
+//           case   3:
+//                System.out.println("Создать характеристики.");
+//                break;
+//
+//            case   4:
+//                System.out.println("Создать объекты.");
+//                break;
+//
+//            case    5:
+//                System.out.println("Задать объектам характеристики.");
+//                break;
+//
+//            case 6:
+//                System.out.println("Запустить систему.");
+//
+//                AtomicInteger bool = new AtomicInteger(-1);
+//
+//                characteristics.forEach(o->{
+//                    System.out.println(String.format("Имеет ли объект %s ?", o));
+//                    bool.set(scannerInt.nextInt());
+//
+//                    objWithСharacteristics.set(objWithСharacteristics.get().entrySet().stream()
+//                            .filter(e -> e.getValue().contains(o))
+//                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+//                });
+//
+//                objWithСharacteristics.get().keySet().forEach(System.out::println);
+//                break;
+//
+//            default: break;
+//        }
+
+    }
+}
